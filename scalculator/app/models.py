d25ce98bb61d9bcb338a94962dd19ab72d8c22c4
simran1agarwal <simranagarwal1912@gmail.com>
2017-06-06 12:04:20 +0530

    models.py modified

--- scalculator/app/models.py
+++ scalculator/app/models.py
@@ -6,9 +6,14 @@ from django.db import models
 '''
 table that contains scholarships and their details.
 '''
+
+
 class Scholarship(models.Model):
     scholarship_name = models.CharField(max_length=1000)
+    '''minimum_cpi = models.DecimalField(max_digits=4, decimal_places=2, default='')'''
 
+    def __str__(self):
+        return self.scholarship_name
 '''
 table that contains details of the user.
 '''
@@ -21,12 +26,15 @@ class Person(models.Model):
     GENDER = (
         ('M', 'Male'),
         ('F', 'Female'),
+        ('O','Other'),
     )
+    gender = models.CharField(max_length=1, choices=GENDER, default='')
     IITG_STUDENT = (
         ('Y', 'yes'),
         ('N', 'no'),
     )
-    family_income = models.IntegerField(max_length=30)
+    IITG_student = models.CharField(max_length=1, choices=IITG_STUDENT, default='N')
+    family_income = models.IntegerField(default="")
     CATEGORY = (
         ('GEN','General'),
         ('SC','Scheduled Class'),
@@ -34,6 +42,7 @@ class Person(models.Model):
         ('PWD','Person With Disability'),
         ('OBC', 'Other Backward Class'),
     )
+    category = models.CharField(max_length=3, choices=CATEGORY, default='GEN')
     PROGRAMME = (
         ('P','Preparatory'),
         ('B1','B.Tech 1st Year'),
@@ -45,9 +54,15 @@ class Person(models.Model):
         ('MS1','MSc 1st Year'),
         ('MS2','MSc 2nd Year'),
     )
-    current_cpi = models.IntegerField(max_length=5)
+    programme = models.CharField(max_length=3, choices=PROGRAMME, default='P')
+    current_cpi = models.DecimalField(default='', decimal_places=2,max_digits=4)
     CITIZEN_INDIA = (
         ('Y','Yes'),
         ('N','No'),
     )
+    citizen_india = models.CharField(max_length=1, choices=CITIZEN_INDIA, default='N')
+
+
+    def __str__(self):
+        return self.person_name + '-' + self.gender
     marked_scholarships = models.ManyToManyField(Scholarship)

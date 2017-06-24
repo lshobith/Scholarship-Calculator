4247ef87a21efe7ef160e9a9990a21daf12c0630
lshobith <lsaishobith@gmail.com>
2017-06-24 00:17:49 +0000

    Clean models.py

--- scalculator/app/models.py
+++ scalculator/app/models.py
@@ -1,46 +1,51 @@
 from django.db import models
 from django.forms import ModelForm
 
-# Create your models here.
-
 
 '''
 table that contains scholarships and their details.
 '''
-
-
 class Scholarship(models.Model):
+
     scholarship_name = models.CharField(max_length=1000)
+
+    maximum_income = models.PositiveIntegerField(default=0)
+
     IITG_SCHOLARSHIP = (
         ('Y', 'Yes'),
         ('N', 'No'),
     )
     iitg_scholarship = models.CharField(max_length=1, choices=IITG_SCHOLARSHIP, default='N')
-    '''minimum_cpi = models.DecimalField(max_digits=4, decimal_places=2, default='')'''
 
-    def __str__(self):
-        return self.scholarship_name
+    #minimum_cpi = models.DecimalField(max_digits=4, decimal_places=2, default='')
+
+
 '''
-table that contains details of the user.
+table that contains profile of the user.
 '''
-
-
 class Person(models.Model):
+
     person_name = models.CharField(max_length=100, null=True)
+
     gmail_id = models.CharField(max_length=50, primary_key=True)
+
     birth_date = models.DateField(null=True)
+
     GENDER = (
         ('M', 'Male'),
         ('F', 'Female'),
         ('O','Other'),
     )
     gender = models.CharField(max_length=1, choices=GENDER, default='')
+
     IITG_STUDENT = (
         ('Y', 'yes'),
         ('N', 'no'),
     )
     IITG_student = models.CharField(max_length=1, choices=IITG_STUDENT, default='N')
+
     family_income = models.IntegerField(default=0)
+
     CATEGORY = (
         ('GEN','General'),
         ('SC','Scheduled Class'),
@@ -49,6 +54,7 @@ class Person(models.Model):
         ('OBC', 'Other Backward Class'),
     )
     category = models.CharField(max_length=3, choices=CATEGORY, default='GEN')
+
     PROGRAMME = (
         ('P','Preparatory'),
         ('B1','B.Tech 1st Year'),
@@ -61,20 +67,22 @@ class Person(models.Model):
         ('MS2','MSc 2nd Year'),
     )
     programme = models.CharField(max_length=3, choices=PROGRAMME, default='P')
+
     current_cpi = models.DecimalField(default=0.00, decimal_places=2,max_digits=4)
+
     CITIZEN_INDIA = (
         ('Y','Yes'),
         ('N','No'),
     )
     citizen_india = models.CharField(max_length=1, choices=CITIZEN_INDIA, default='N')
 
-
-    def __str__(self):
-        return self.person_name + '-' + self.gender
     marked_scholarships = models.ManyToManyField(Scholarship)
 
+
 '''
-form for Person class
+form for Person class.
+this is not a database.
+this form is used for editing the database.
 '''
 class PersonForm(ModelForm):
     class Meta:
